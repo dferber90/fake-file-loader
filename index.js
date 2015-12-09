61f65a3b6990eff99e9ef79c438e42005c64b234
Dominik Ferber <dominik.ferber@gmail.com>
2015-12-09 21:03:23 +0100

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+var loaderUtils = require("loader-utils");
+
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+  var query = loaderUtils.parseQuery(this.query);
+  var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
+    context: query.context || this.options.context,
+    content: content,
+    regExp: query.regExp
+  });
+  return "module.exports = __webpack_public_path__ + " + JSON.stringify(url) + ";";
+}
+module.exports.raw = true;
